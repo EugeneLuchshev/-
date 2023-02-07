8a2e9078afc7c0600052299b69e83acd269c6501
Пользователь <john7243@mail.ru>
2023-02-07 22:48:14 +0700

    scanner fileV3

--- Сканнер код V2.py
+++ Сканнер код V2.py
@@ -1,15 +1,13 @@
-import sys
+
 barcode = ' '
 print('отсканируй штрихкод')
-while barcode: #создаем бесконечный цикл
+while True: #создаем бесконечный цикл
     barcode = str(input()) #переменная, куда будут вводиться отсканированные штрихкоды
     print("отсканирован:", barcode)
-    try:
-        with open('text.txt', 'a') as file: # записываем отсканированные значения в тхт файл
-            file.write('штрихкод:')
-            file.write(barcode)
-            file.write('\n') #отступ между 'штрихкод: ' и отсканированным значением
-    except:
+    with open('text.txt', 'a') as file: # записываем отсканированные значения в тхт файл
+        file.write('штрихкод:')
+        file.write(barcode)
+        file.write('\n') #отступ между 'штрихкод: ' и отсканированным значением
         print('штрихкод сохранен')
 
 
